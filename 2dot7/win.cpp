c7115dcd5acfa2dc40013e4ab460918b0d8d42eb
Stilb0n <stilbon01@gmail.com>
2024-04-16 13:14:58 +0700

    убрал кодек и добавил комментарии

--- 2dot7/win.cpp
+++ 2dot7/win.cpp
@@ -6,41 +6,36 @@
 win::win(QWidget *parent)
     : QWidget(parent)
 {
-    codec = QTextCodec::codecForName("Windows-1251");
-    setWindowTitle(codec->toUnicode("Возведение в квадрат"));
+
+    setWindowTitle("Возведение в квадрат");
     frame = new QFrame(this);
-    frame->setFrameShadow(QFrame::Raised);
-    frame->setFrameShape(QFrame::Panel);
-    inputLabel = new QLabel(codec->toUnicode("Введите число:"),
-                            this);
+    frame->setFrameShadow(QFrame::Raised); //тень будет поднятой
+    frame->setFrameShape(QFrame::Panel); //рамка будет прямоугольной с простым угловым соединением
+    inputLabel = new QLabel("Введите число:",this); //метка для текста над редактором ввода
     inputEdit = new QLineEdit("",this);
-    StrValidator *v=new StrValidator(inputEdit);
-    inputEdit->setValidator(v);
-    outputLabel = new QLabel(codec->toUnicode("Результат:"),
-                             this);
-    outputEdit = new QLineEdit("",this);
-    nextButton = new QPushButton(codec->toUnicode("Следующее"),
- this);
-    exitButton = new QPushButton(codec->toUnicode("Выход"),
- this);
+    //Передача указателя на объект QLineEdit в конструктор StrValidator позволяет связать этот валидатор с конкретным полем ввода и применить к нему заданные правила валидации.
+    StrValidator *v=new StrValidator(inputEdit); // передается указатель на объект QLineEdit в качестве аргумента конструктора
+    inputEdit->setValidator(v); // устанавливаем валидатор для редактора ввода
+    outputLabel = new QLabel("Результат:",this); // метка для текста перед редактором вывода
+    outputEdit = new QLineEdit("",this); // редактор вывода
+    nextButton = new QPushButton("Следующее", this); // кнопка для ввода следующего числа
+    exitButton = new QPushButton("Выход",this); //кнопка выхода из приложения
     // компоновка приложения
-    QVBoxLayout *vLayout1 = new QVBoxLayout(frame);
+    QVBoxLayout *vLayout1 = new QVBoxLayout(frame); //Класс QVBoxLayout Выстраивает виджеты в вертикальную линию
     vLayout1->addWidget(inputLabel);
     vLayout1->addWidget(inputEdit);
     vLayout1->addWidget(outputLabel);
-    vLayout1->addWidget(outputEdit);
-    //при растягивании окна объекты держатся вместе
-    vLayout1->addStretch();
-    QVBoxLayout *vLayout2 = new QVBoxLayout();
+    vLayout1->addWidget(outputEdit); 
+    vLayout1->addStretch(); //при растягивании окна объекты держатся вместе
+    QVBoxLayout *vLayout2 = new QVBoxLayout(); // еще один объект класса QVBoxLayout, но в данном случае он создается без родительского виджета. Для особой рамки была только у ввода и вывода.
     vLayout2->addWidget(nextButton);
     vLayout2->addWidget(exitButton);
-    // почему тут 2 пружины?
     vLayout2->addStretch();
-    QHBoxLayout *hLayout = new QHBoxLayout(this);
+    QHBoxLayout *hLayout = new QHBoxLayout(this); //Класс QVBoxLayout Выстраивает виджеты в горизонтальную линию
     hLayout->addWidget(frame);
     hLayout->addLayout(vLayout2);
-    begin();
-    //рассказать про функционал connect
+    begin(); //слот, метод начальной настройки интерфейса
+
     // Подключение сигнала и слота для кнопки выхода
     connect(exitButton, &QPushButton::clicked, this, &win::close);
 
@@ -55,42 +50,42 @@ win::win(QWidget *parent)
 
 void win::begin()
 {
-    inputEdit->clear();
-    nextButton->setEnabled(false);
-    nextButton->setDefault(false);
-    inputEdit->setEnabled(true);
-    outputLabel->setVisible(false);
-    outputEdit->setVisible(false);
-    outputEdit->setEnabled(false);
-    inputEdit->setFocus();
+    inputEdit->clear(); //очищаем редакор ввода
+    nextButton->setEnabled(false); // делаем неактивной кнопку далее
+    nextButton->setDefault(false); // отключаем автоматическое нажатие кнопки при нажатии кнопки Enter
+    inputEdit->setEnabled(true);// делаем активным редактор ввода
+    outputLabel->setVisible(false); // делаем невидимым метку вывода
+    outputEdit->setVisible(false); // делаем невидимым редактор вывода
+    outputEdit->setEnabled(false); // делаем неактивным редактор вывода
+    inputEdit->setFocus(); // Предоставлям фокус ввода с клавиатуры редактору ввода
 }
-
+// метод реализации вычислений
 void win::calc()
 {
     bool Ok=true; float r,a;
-    QString str=inputEdit->text();
-    //передаем адрес если бы не адрес то не скомпилировался бы (!передаем переменную по адресу для того чтобы функция могла её изменить!.)
+    QString str=inputEdit->text(); //переносим текст в переменную str из редактора ввода
+    //передаем адрес если бы не адрес то не скомпилировался бы (! далее идёт неверное утверждение -> передаем переменную по адресу для того чтобы функция могла её изменить!.)
     a=str.toDouble(&Ok);
-    if ((Ok)&&(a<1000))
+    if ((Ok)&&(a<1000)) // если введены символы приемлимого типа,то есть числа то происходят дальнейшие вычисления так же если число меньше 1000
     {
-        r=a*a;
-        str.setNum(r);
-        outputEdit->setText(str);
-        inputEdit->setEnabled(false);
-        outputLabel->setVisible(true);
-        outputEdit->setVisible(true);
-        nextButton->setDefault(true);
-        nextButton->setEnabled(true);
-        nextButton->setFocus();
+        r=a*a; //возведение в квадрат
+        str.setNum(r); // применение изменений к str
+        outputEdit->setText(str); // помещаем str в редактор вывода
+        inputEdit->setEnabled(false); //деактивируется редактор ввода
+        outputLabel->setVisible(true); // активируется видимость метки вывода
+        outputEdit->setVisible(true); // активируется видимость редактора вывода
+        nextButton->setDefault(true); //активируется принятие нажатия кнопкой энтера кнопки следующее
+        nextButton->setEnabled(true); //активируется возможность нажать кнопку следующее
+        nextButton->setFocus(); // ставится фокусировка на кнопку следующее
     }
-    else
-        if (!str.isEmpty())
+    else // если введены символы неприемлимого типа,то есть числа то происходят дальнейшие вычисления так же если число больше или равно 1000
+        if (!str.isEmpty()) // дополнительная проверка если строка не пустая
         {
             QMessageBox msgBox(QMessageBox::Information,
-                               codec->toUnicode("Возведение в квадрат."),
-                               codec->toUnicode("Введено неверное значение."),
-                               QMessageBox::Ok);
-            msgBox.exec();
+                               ("Возведение в квадрат."),
+                              ("Введено неверное значение."),
+                               QMessageBox::Ok); //инициализируем объект типа информационного окна, который уведомляет о ситуации
+            msgBox.exec(); // делает окно модальным, Модальным называется окно, которое блокирует работу пользователя с родительским приложением до тех пор, пока пользователь это окно не закроет.
         }
 }
 
--- build-2dot7-Desktop_Qt_6_5_0_MinGW_64_bit-Debug/debug/moc_win.cpp
+++ build-2dot7-Desktop_Qt_6_5_0_MinGW_64_bit-Debug/debug/moc_win.cpp
@@ -40,20 +40,32 @@ namespace {
 #ifdef QT_MOC_HAS_STRINGDATA
 struct qt_meta_stringdata_CLASSwinENDCLASS_t {};
 static constexpr auto qt_meta_stringdata_CLASSwinENDCLASS = QtMocHelpers::stringData(
-    "win"
+    "win",
+    "begin",
+    "",
+    "calc"
 );
 #else  // !QT_MOC_HAS_STRING_DATA
 struct qt_meta_stringdata_CLASSwinENDCLASS_t {
-    uint offsetsAndSizes[2];
+    uint offsetsAndSizes[8];
     char stringdata0[4];
+    char stringdata1[6];
+    char stringdata2[1];
+    char stringdata3[5];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_CLASSwinENDCLASS_t::offsetsAndSizes) + ofs), len 
 Q_CONSTINIT static const qt_meta_stringdata_CLASSwinENDCLASS_t qt_meta_stringdata_CLASSwinENDCLASS = {
     {
-        QT_MOC_LITERAL(0, 3)   // "win"
+        QT_MOC_LITERAL(0, 3),  // "win"
+        QT_MOC_LITERAL(4, 5),  // "begin"
+        QT_MOC_LITERAL(10, 0),  // ""
+        QT_MOC_LITERAL(11, 4)   // "calc"
     },
-    "win"
+    "win",
+    "begin",
+    "",
+    "calc"
 };
 #undef QT_MOC_LITERAL
 #endif // !QT_MOC_HAS_STRING_DATA
@@ -65,12 +77,20 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSwinENDCLASS[] = {
       11,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   26,    2, 0x0a,    1 /* Public */,
+       3,    0,   27,    2, 0x0a,    2 /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -83,16 +103,26 @@ Q_CONSTINIT const QMetaObject win::staticMetaObject = { {
     nullptr,
     qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSwinENDCLASS_t,
         // Q_OBJECT / Q_GADGET
-        QtPrivate::TypeAndForceComplete<win, std::true_type>
+        QtPrivate::TypeAndForceComplete<win, std::true_type>,
+        // method 'begin'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'calc'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
     nullptr
 } };
 
 void win::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    (void)_o;
-    (void)_id;
-    (void)_c;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<win *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->begin(); break;
+        case 1: _t->calc(); break;
+        default: ;
+        }
+    }
     (void)_a;
 }
 
@@ -112,6 +142,17 @@ void *win::qt_metacast(const char *_clname)
 int win::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 2;
+    }
     return _id;
 }
 QT_WARNING_POP
